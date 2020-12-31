bd36a48dc91a8fba969a3cb92cd3bdde18c7e606
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-12-31 15:17:45 -0300

    test: add layout unit tests

---
+++ src/components/layout/layout.spec.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import Layout from './';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Layout />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
